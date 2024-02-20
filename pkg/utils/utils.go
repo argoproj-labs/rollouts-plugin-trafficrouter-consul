5f9ea50e584aec43a4a164a773293fc7e3fb4ceb
Michael Wilkerson <mwilkerson@hashicorp.com>
2024-02-20 15:19:29 -0800

    removed slogger, we don't use this at all
    Signed-off-by: Michael Wilkerson <mwilkerson@hashicorp.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,10 +1,6 @@
 package utils
 
 import (
-	"os"
-
-	"log/slog"
-
 	pluginTypes "github.com/argoproj/argo-rollouts/utils/plugin/types"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -16,20 +12,4 @@ func NewKubeConfig() (*rest.Config, error) {
 		return nil, pluginTypes.RpcError{ErrorString: err.Error()}
 	}
 	return config, nil
-}
-
-func InitLogger(lvl slog.Level) {
-	lvlVar := &slog.LevelVar{}
-	lvlVar.Set(lvl)
-	opts := slog.HandlerOptions{
-		Level: lvlVar,
-	}
-
-	attrs := []slog.Attr{
-		slog.String("plugin", "trafficrouter"),
-		slog.String("vendor", "consul"),
-	}
-
-	l := slog.New(slog.NewTextHandler(os.Stderr, &opts).WithAttrs(attrs))
-	slog.SetDefault(l)
 }
